3ff98387fd7c57953de79aba92c5c5e57970f012
KexinFeng <23562091+KexinFeng@users.noreply.github.com>
2025-03-13 11:42:06 +0800

    plot_Gtau_J

--- qed_fermion/plot_local_hmc_Gtau_J.py
+++ qed_fermion/plot_local_hmc_Gtau_J.py
@@ -1,5 +1,6 @@
 import json
 import math
+import re
 import matplotlib.pyplot as plt
 plt.ion()
 
@@ -15,11 +16,11 @@ import torch
 import sys
 sys.path.insert(0, script_path + '/../')
 
-from qed_fermion.hmc_sampler2_batch_fermion import HmcSampler
-from qed_fermion.hmc_sampler2_batch_local import LocalUpdateSampler
+from qed_fermion.hmc_sampler_batch import HmcSampler
+from qed_fermion.local_sampler_batch import LocalUpdateSampler
 
 
-def load_visualize_final_greens_loglog(Lsize=(20, 20, 20), hmc_filename='', dqmc_filename='', local_update_filename='', specifics = '', starts=[500], ends=[1000001], sample_steps=[1], scale_it=[False]):
+def load_visualize_final_greens_loglog(Lsize=(20, 20, 20), hmc_filename='', local_update_filename='', specifics = '', starts=[500], sample_steps=[1], scale_it=[False]):
     """
     Visualize green functions with error bar
     """
@@ -28,28 +29,29 @@ def load_visualize_final_greens_loglog(Lsize=(20, 20, 20), hmc_filename='', dqmc
     # Lx, Ly, Ltau = 20, 20, 20
     Lx, Ly, Ltau = Lsize
 
-
     idx_ref = 5
 
     # Parse specifics
     parts = specifics.split('_')
     jtau_index = parts.index('Jtau')  # Find position of 'Jtau'
     jtau_value = float(parts[jtau_index + 1])   # Get the next element
-    
 
     # ======== Plot ======== #
     plt.figure()
     if len(hmc_filename):
+        res = torch.load(hmc_filename)
+        print(f'Loaded: {hmc_filename}')        
+        
+        # Extract Nstep and Nstep_local from filenames
+        hmc_match = re.search(r'Nstp_(\d+)', hmc_filename)
+        end = int(hmc_match.group(1))
+
         start = starts.pop(0)
-        end = ends.pop(0)
         sample_step = sample_steps.pop(0)
         seq_idx = np.arange(start, end, sample_step)
 
-        res = torch.load(hmc_filename)
-        print(f'Loaded: {hmc_filename}')
 
         G_list = res['G_list']
-        step = res['step']
         x = np.array(list(range(G_list[0].size(-1))))
 
         G_mean = G_list[seq_idx].numpy().mean(axis=(0, 1))
@@ -57,44 +59,29 @@ def load_visualize_final_greens_loglog(Lsize=(20, 20, 20), hmc_filename='', dqmc
         plt.errorbar(x, G_list[seq_idx].numpy().mean(axis=(0, 1)), yerr=G_list[seq_idx].numpy().std(axis=(0, 1))/np.sqrt(seq_idx.size), linestyle='-', marker='o', label='G_hmc', color='blue', lw=2)
 
 
-    if len(dqmc_filename):   
-        # name = f"l4b3js{jtau_value:.1f}jpi1.0mu0.0nf2_dqmc_bin.dat"
-        data = np.genfromtxt(dqmc_filename)
-        G_dqmc = np.concat([data[:, 0], data[:1, 0]])
-        G_dqmc_err = np.concat([data[:, 1], data[:1, 1]])
-        
-        if scale_it.pop(0):
-            ## Scale G_mean_anal to match G_mean based on their first elements
-            scale_factor = G_mean[idx_ref] / G_dqmc[idx_ref]
-            G_dqmc *= scale_factor
-        
-        x_dqmc = np.array(list(range(G_dqmc.size)))
-        plt.errorbar(x_dqmc, G_dqmc, yerr=G_dqmc_err, linestyle='--', marker='*', label='G_dqmc', color='red', lw=2, ms=10)
-
-
     if len(local_update_filename): 
         res = torch.load(local_update_filename)
         print(f'Loaded: {local_update_filename}')
 
-        G_list_local = res['G_list']
-        step_local = res['step']
-        x_local = np.array(list(range(G_list_local[0].size(-1))))
-
+        # Extract Nstep and Nstep_local from filenames
+        local_match = re.search(r'Nstp_(\d+)', local_update_filename)
+        end_local = int(local_match.group(1))
         start_local = starts.pop(0)
-        end_local = ends.pop(0)
         sample_step_local = sample_steps.pop(0)
         seq_idx_local = np.arange(start_local, end_local, sample_step_local)
 
-        G_local_mean = G_list_local[seq_idx_local].numpy().mean(axis=(0, 1))
-        G_local_std = G_list_local[seq_idx_local].numpy().std(axis=(0, 1))
+        G_list_local = res['G_list']
+        x_local = np.array(list(range(G_list_local[0].size(-1))))
+
+        batch_idx = torch.tensor([0, 1, 4])
+        G_local_mean = G_list_local[seq_idx_local][:, batch_idx].numpy().mean(axis=(0, 1))
+        G_local_std = G_list_local[seq_idx_local][:, batch_idx].numpy().std(axis=(0, 1))
 
         if scale_it.pop(0):
-            # scale_factor = G_dqmc[idx_ref] / G_local_mean[idx_ref] if len(dqmc_filename) else 1
             scale_factor = G_mean[idx_ref] / G_local_mean[idx_ref] if len(hmc_filename) else 1
             G_local_mean *= scale_factor
         
-        plt.errorbar(x_local, G_local_mean, yerr=G_local_std/np.sqrt(seq_idx_local.size), linestyle='-', marker='o', label='G_local', color='green', lw=2)
-
+        plt.errorbar(x_local, G_local_mean, yerr=G_local_std/np.sqrt(seq_idx_local.size), linestyle='-', marker='o', label=f'G_local_{batch_idx.tolist()}', color='green', lw=2)
 
     # Add labels and title
     plt.xlabel(r"$\tau$")
@@ -105,24 +92,19 @@ def load_visualize_final_greens_loglog(Lsize=(20, 20, 20), hmc_filename='', dqmc
     # Save plot
     class_name = __file__.split('/')[-1].replace('.py', '')
     method_name = "greens"
-    save_dir = os.path.join(script_path, f"./figures/figure_{class_name}")
+    save_dir = os.path.join(script_path, f"./figures/{class_name}")
     os.makedirs(save_dir, exist_ok=True) 
     file_path = os.path.join(save_dir, f"{method_name}_{specifics}.pdf")
     plt.savefig(file_path, format="pdf", bbox_inches="tight")
     print(f"Figure saved at: {file_path}")
-
 
     # ======== Log plot ======== #
     plt.figure()
     if len(hmc_filename):
         plt.errorbar(x+1, G_list[seq_idx].numpy().mean(axis=(0, 1)), yerr=G_list[seq_idx].numpy().std(axis=(0, 1))/np.sqrt(seq_idx.size), linestyle='', marker='o', label='G_hmc', color='blue', lw=2)
 
-    if len(dqmc_filename):
-        plt.errorbar(x+1, G_dqmc, yerr=G_dqmc_err, linestyle='--', marker='*', label='G_dqmc', color='red', lw=2, ms=10)
-
     if len(local_update_filename):
         plt.errorbar(x_local+1, G_local_mean, yerr=G_local_std/np.sqrt(seq_idx_local.size), linestyle='', marker='o', label='G_local', color='green', lw=2)
-
 
     # Add labels and title
     plt.xlabel('X-axis label')
@@ -133,22 +115,16 @@ def load_visualize_final_greens_loglog(Lsize=(20, 20, 20), hmc_filename='', dqmc
     plt.xscale('log')
     plt.yscale('log')
 
-
     # --------- save_plot ---------
     class_name = __file__.split('/')[-1].replace('.py', '')
     method_name = "greens_log"
-    save_dir = os.path.join(script_path, f"./figures/figure_{class_name}")
+    save_dir = os.path.join(script_path, f"./figures/{class_name}")
     os.makedirs(save_dir, exist_ok=True) 
     file_path = os.path.join(save_dir, f"{method_name}_{specifics}.pdf")
     plt.savefig(file_path, format="pdf", bbox_inches="tight")
     print(f"Figure saved at: {file_path}")
 
-
-
 if __name__ == '__main__':
-    # J = float(os.getenv("J"))
-    # Nstep = int(os.getenv("Nstep"))
-
     Js = [0.5]
 
     for J in Js:
@@ -164,19 +140,28 @@ if __name__ == '__main__':
         lmc.reset()
 
         # File names
-        # hmc
-        step = Nstep
-        hmc_filename = script_path + f"/check_points/hmc_check_point/ckpt_N_{hmc.get_specifics()}_step_{step}.pt"
+        # # hmc
+        # step = Nstep
+        # hmc_filename = script_path + f"/check_points/hmc_check_point/ckpt_N_{hmc.get_specifics()}_step_{step}.pt"
         
-        # local
-        step_lmc = lmc.N_step
-        local_update_filename = script_path + f"/check_points/local_check_point/ckpt_N_{lmc.get_specifics()}_step_{step_lmc}.pt"
+        # # local
+        # step_lmc = lmc.N_step
+        # local_update_filename = script_path + f"/check_points/local_check_point/ckpt_N_{lmc.get_specifics()}_step_{step_lmc}.pt"
+
+        hmc_filename = script_path + "/check_points/hmc_check_point_m*0.05/ckpt_N_hmc_6_Ltau_10_Nstp_10000_Jtau_0.5_K_1_dtau_0.1_step_10000.pt"
+        local_update_filename = script_path + "/check_points/local_check_point/ckpt_N_local_6_Ltau_10_Nstp_3600000_Jtau_0.5_K_1_dtau_0.1_step_3600000.pt"
 
         # Measure
         Lx, Ly, Ltau = hmc.Lx, hmc.Ly, hmc.Ltau
-        load_visualize_final_greens_loglog((Lx, Ly, Ltau), hmc_filename, '', local_update_filename, specifics=hmc.get_specifics(), starts=[500, 200*lmc.Vs], ends=[Nstep, step_lmc], sample_steps=[1, 10], scale_it=[False, False])
+        load_visualize_final_greens_loglog(
+            (Lx, Ly, Ltau), 
+            hmc_filename, local_update_filename, 
+            specifics=hmc.get_specifics(), 
+            starts=[500, 1000*lmc.Vs], 
+            sample_steps=[1, lmc.Vs], 
+            scale_it=[False, False])
 
-        plt.show(block=False)
+    plt.show(block=True)
 
     dbstop = 1
 
