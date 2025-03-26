9bef7d1adf74d62fc83ce252e8b8c7285f9c917b
kxf <fengx463@alumni.umn.edu>
2025-03-27 00:18:07 +0800

    run_config

--- qed_fermion/pbs_files/r.sh
+++ qed_fermion/pbs_files/r.sh
@@ -4,33 +4,33 @@ mkdir -p report
 J_array=$(echo '3 1 0.5')
 # J_array=$(echo '3')
 
-Nstep=10000
-for J in $J_array; do
-        #
-        config=$(echo hmc_J_${J}_Nstep_${Nstep})
-        echo $config
-        export J Nstep
-        #
-        sbatch --job-name=${config}_hmc \
-        --time=0-4:00:00 \
-        --qos=normal \
-        --mem-per-cpu=6G \
-        s_hmc.cmd
-done
-
-# Nstep=5000
-# bs=5
+# Nstep=10000
 # for J in $J_array; do
 #         #
-#         config=$(echo local_J_${J}_Nstep_${Nstep}_bs_${bs})
+#         config=$(echo hmc_J_${J}_Nstep_${Nstep})
 #         echo $config
-#         export J Nstep bs
+#         export J Nstep
 #         #
 #         sbatch --job-name=${config}_hmc \
-#         --time=0-20:00:00 \
+#         --time=0-4:00:00 \
 #         --qos=normal \
 #         --mem-per-cpu=6G \
-#         s_local.cmd
+#         s_hmc.cmd
 # done
+
+Nstep=3000
+bs=5
+for J in $J_array; do
+        #
+        config=$(echo local_J_${J}_Nstep_${Nstep}_bs_${bs})
+        echo $config
+        export J Nstep bs
+        #
+        sbatch --job-name=${config}_hmc \
+        --time=0-16:00:00 \
+        --qos=gpu \
+        --mem-per-cpu=6G \
+        s_local.cmd
+done
 
 # qos=normal
