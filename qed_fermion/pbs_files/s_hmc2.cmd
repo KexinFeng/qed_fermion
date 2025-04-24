149942f07dd8c0a54267b4ff35b1da44bc6c4fa3
kxf <fengx463@alumni.umn.edu>
2025-04-24 22:11:24 +0800

    run_config

---
+++ qed_fermion/pbs_files/s_hmc2.cmd
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-type=END,FAIL    # 2. Send email upon events (Options: NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=fengx463@hku.hk     #    Email address to receive notification
+#SBATCH --partition=amd
+##SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=./report/%x_%j.out            # 8. Standard output log as $job_name_$job_id.out
+#SBATCH --error=./report/%x_%j.err             #    Standard error log as $job_name_$job_id.errs
+
+cd ${SLURM_SUBMIT_DIR}
+module load python/3.9.7
+module load matlab/r2023b
+export LD_LIBRARY_PATH=/home/fengx463/Downloads:$LD_LIBRARY_PATH
+
+python3 ../hmc_sampler_batch.py
+# python3 -c "import\ matlab.engine"
