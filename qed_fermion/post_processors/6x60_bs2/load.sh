c10e1b3eb4020259e644ba226a39c968f8395acb
KexinFeng <23562091+KexinFeng@users.noreply.github.com>
2025-05-21 22:11:49 +0800

    postprocessing

---
+++ qed_fermion/post_processors/6x60_bs2/load.sh
@@ -0,0 +1,14 @@
+cd /Users/kx/Desktop/hmc/qed_fermion/qed_fermion/post_processors/6x60_bs2
+export Lx=6
+export Ly=60
+python3 load_write2file_convert.py
+
+export Lx=8
+export Ly=80
+python3 load_write2file_convert.py
+
+export Lx=10
+export Ly=100
+python3 load_write2file_convert.py
+
+
