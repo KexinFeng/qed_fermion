0616d8f9c7c02295269cb88f8d3ba00cb4cb8cb9
KexinFeng <23562091+KexinFeng@users.noreply.github.com>
2025-05-27 15:55:21 +0800

    assertion

---
+++ qed_fermion/post_processors/fermi_bench/compare_assertion.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+
+plt.ion()
+
+import numpy as np
+import math
+
+from matplotlib import rcParams
+rcParams['figure.raise_window'] = False
+
+import os
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+import torch
+import sys
+sys.path.insert(0, script_path + '/../../../')
+
+from load_write2file_convert import time_execution
+bs = 5
+data_list = []
+
+for bid in range(bs):
+    file_path = f"/Users/kx/Desktop/forked/dqmc_u1sl_mag/run_benchmark/run_meas_J_0.5_L_6_Ltau_10_bid{bid}_part_0_psz_500_start_5999_end_6000/spsm.bin"
+    # Assuming torch.load is appropriate for loading the file
+    data = np.genfromtxt(file_path)
+    
+    txt_file_path = f"/Users/kx/Desktop/hmc/qed_fermion/qed_fermion/post_processors/fermi_bench/spsm_k_b{bid}.txt"
+    data2 = np.genfromtxt(txt_file_path)
+
+    # Compare the third column of data and data2 using torch.testing.assert_close
+    torch.testing.assert_close(
+        torch.tensor(data[:, 2]), 
+        torch.tensor(data2[:, 2]), 
+        atol=3e-2, 
+        rtol=0
+    )
+
+
+
+
+
+
