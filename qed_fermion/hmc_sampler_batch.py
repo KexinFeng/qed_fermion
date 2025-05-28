a0284c7409be8814927ac87e17f92babb0cc7daa
kexin <fengx463@alumni.umn.edu>
2025-05-28 10:42:14 -0500

    max_tau_block_idx = 5

--- qed_fermion/hmc_sampler_batch.py
+++ qed_fermion/hmc_sampler_batch.py
@@ -100,7 +100,7 @@ class HmcSampler(object):
         self.Vs = self.Lx * self.Ly
         self.tau_block_idx = 0
         asym = self.Ltau // self.Lx // 10
-        self.max_tau_block_idx = 1 if asym > 0 else 1
+        self.max_tau_block_idx = 5 if asym > 0 else 1
         print(f"max_tau_block_idx: {self.max_tau_block_idx}")    
         self.tau_block_size = self.Ltau // self.max_tau_block_idx
         dt_deque_max_len = 5 * self.max_tau_block_idx
