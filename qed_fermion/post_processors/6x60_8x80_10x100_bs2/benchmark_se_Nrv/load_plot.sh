375843f55497848424ba1be2b69cc4e5fef6edd0
kxf <fengx463@alumni.umn.edu>
2025-06-02 01:22:51 +0800

    plot_Nrv200

---
+++ qed_fermion/post_processors/6x60_8x80_10x100_bs2/benchmark_se_Nrv/load_plot.sh
@@ -0,0 +1,33 @@
+cd "$(dirname "$0")"
+export cuda_graph=1
+
+export Lx=6
+export Ltau=60
+python3 load_se.py
+
+export Lx=8
+export Ltau=80
+python3 load_se.py
+
+export Lx=10
+export Ltau=100
+python3 load_se.py
+
+## Plot
+# export Lx=6
+# export Ltau=60
+# python3 plot_local_hmc_energy_J_self_dqmc.py
+# python3 plot_local_hmc_energy_J_dqmc_vs_hmc.py
+# python3 plot_spsm_dqmc_vs_hmc.py
+
+# export Lx=8
+# export Ltau=80
+# python3 plot_local_hmc_energy_J_self_dqmc.py
+# python3 plot_local_hmc_energy_J_dqmc_vs_hmc.py
+# python3 plot_spsm_dqmc_vs_hmc.py
+
+# export Lx=10
+# export Ltau=100
+# python3 plot_local_hmc_energy_J_self_dqmc.py
+# python3 plot_local_hmc_energy_J_dqmc_vs_hmc.py
+# python3 plot_spsm_dqmc_vs_hmc.py
---
+++ qed_fermion/post_processors/6x60_8x80_10x100_bs2/benchmark_se_Nrv/load_se.py
@@ -0,0 +1,122 @@
+import glob
+import json
+import math
+import re
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+plt.ion()
+import numpy as np
+# matplotlib.use('MacOSX')
+from matplotlib import rcParams
+rcParams['figure.raise_window'] = False
+import os
+script_path = os.path.dirname(os.path.abspath(__file__))
+import torch
+import sys
+sys.path.insert(0, script_path + '/../../../../')
+import time
+from qed_fermion.hmc_sampler_batch import HmcSampler
+from qed_fermion.stochastic_estimator import StochaticEstimator
+
+def time_execution(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Execution time for {func.__name__}: {end_time - start_time:.2f} seconds\n")
+        return result
+    return wrapper
+
+
+def postprocess_and_write_spsm(bosons, output_dir, Lx, Ly, Ltau, Nrv=10, start=5000):
+    """
+    boson: [seq, J/bs, 2 * Lx * Ly * Ltau]
+    """
+    hmc = HmcSampler()
+    hmc.Lx = Lx
+    hmc.Ly = Ly
+    hmc.Ltau = Ltau
+    hmc.bs = bosons.shape[1]
+    hmc.reset()
+
+    se = StochaticEstimator(hmc, cuda_graph_se=hmc.cuda_graph)
+    se.Nrv = Nrv
+    if se.cuda_graph_se:
+        se.init_cuda_graph()
+    eta = se.random_vec_bin()
+    os.makedirs(output_dir, exist_ok=True)
+    boson_conf = bosons.view(bosons.shape[0], bosons.shape[1], 2, Lx, Ly, Ltau)[start:]
+    spsm_k = []
+    for boson in tqdm(boson_conf):  # boson: [J/bs, 2, Lx, Ly, Ltau]
+        if se.cuda_graph_se:
+            obsr = se.graph_runner(boson.to(se.device), eta)
+        else:
+            obsr = se.get_fermion_obsr(boson.to(se.device), eta)
+        spsm_k.append(obsr['spsm_k_abs'].cpu().numpy())
+        
+    spsm_k = np.array(spsm_k)  # [seq, J/bs, Ly, Lx]
+    spsm_k_mean = spsm_k.mean(axis=0)  # [J/bs, Ly, Lx]
+    spsm_k_std = spsm_k.std(axis=0)  # [J/bs, Ly, Lx]
+
+    output_file = os.path.join(output_dir, "spsm_k.pt")
+    torch.save({'mean': spsm_k_mean, 
+                'std': spsm_k_std}, 
+                output_file)
+    print(f"Saved: {output_file}")
+
+    # ks = se.get_ks_ordered().cpu().numpy()
+    # ks = ks.reshape(-1, 2)[:, ::-1]
+    # for b in range(spsm_k_mean.shape[0]):
+    #     data = np.stack([ks[:, 0], ks[:, 1], spsm_k_mean[b].reshape(-1)], axis=1)
+    #     output_file = os.path.join(output_dir, f"spsm_k_b{b}.txt")
+    #     np.savetxt(output_file, data, fmt="%.8f", comments='')
+    #     print(f"Saved: {output_file}")
+    
+
+if __name__ == '__main__':
+    # Create configs file
+    Lx = int(os.getenv("Lx", '8'))
+    print(f"Lx: {Lx}")
+    Ltau = int(os.getenv("Ltau", '80'))
+    print(f"Ltau: {Ltau}")
+    # Ltau = Lx * 10
+
+    Js = [1.0, 1.5, 2.0, 2.3, 2.5, 3.0]
+    # Js = [0.5, 1.0, 3.0]
+
+    bs = 2
+
+    input_folder = "/Users/kx/Desktop/hmc/fignote/equilibrium_issue/hmc_check_point_bench/"
+    input_folder = "/users/4/fengx463/hmc/fignote/equilibrum_issue/"
+    input_folder = "/home/fengx463/hmc/qed_fermion/qed_fermion/check_points/hmc_check_point_bench/"
+
+    start = 5000
+    end = 10000
+
+    @time_execution
+    def iterate_func():
+        bosons = []
+        for J in Js:
+
+            bid = 1
+
+            hmc_filename = f"/stream_ckpt_N_t_hmc_{Lx}_Ltau_{Ltau}_Nstp_{end}_bs{bs}_Jtau_{J:.2g}_K_1_dtau_0.1_delta_t_0.028_N_leapfrog_5_m_1_cg_rtol_1e-09_max_block_idx_1_gear0_steps_1000_dt_deque_max_len_5_step_{end}.pt"
+
+            # Load and write
+            # [seq, bs, 2*Lx*Ly*Ltau]
+            boson_seq = torch.load(input_folder + hmc_filename)
+            # boson_seq = boson_seq.to(device='mps', dtype=torch.float32)
+            print(f'Loaded: {hmc_filename}')  
+            
+            # Post-process and write spsm
+            Ly = Lx  # Assuming square lattice, adjust if not
+            
+            bosons.append(boson_seq[:, bid])
+
+        bosons = torch.stack(bosons, dim=1)  # [seq, J/bs, 2*Lx*Ly*Ltau]
+        
+        output_dir = script_path + f"/Lx_{Lx}_Ltau_{Ltau}/"
+        postprocess_and_write_spsm(bosons, output_dir, Lx, Ly, Ltau, Nrv=200, start=start)
+            
+  
+    iterate_func()
