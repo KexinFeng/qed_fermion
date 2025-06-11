dc99ece31bd027488de00f1ca46f9a0fff30f884
kxf <fengx463@alumni.umn.edu>
2025-06-11 10:08:06 +0800

    Nrv_test

---
+++ qed_fermion/post_processors/6x60_8x80_10x100_bs2/benchmark_se_Nrv30/r.sh
@@ -0,0 +1,32 @@
+mkdir -p report_se
+
+
+Nrv_array=$(echo '30')
+L_array=$(echo '6 8')  # 10 h (-2)
+
+export debug=0
+export cuda_graph=1
+export mxitr=200  # 1000
+
+for Nrv in $Nrv_array; do
+        #
+        for Lx in $L_array; do
+                #
+                config=$(echo Lx${Lx}Nrv${Nrv}mxitr${mxitr})
+                echo $config
+                export Nrv Lx
+                export Ltau=$((10 * Lx))
+                sbatch --job-name=${config} \
+                        --time=0-1:00:00 \
+                        --qos=gpu \
+                        --mem-per-cpu=18G \
+                        s_hmc.cmd
+        done
+done
+
+# sbatch --job-name=se \
+#         --time=0-3:00:00 \
+#         --qos=gpu \
+#         --mem-per-cpu=10G \
+#         s_hmc.cmd
+   
