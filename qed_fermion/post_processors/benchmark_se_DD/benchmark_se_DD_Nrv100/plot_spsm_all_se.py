b93ce1e2f069aacc80e7f59133ac0b5542fbe46a
KexinFeng <23562091+KexinFeng@users.noreply.github.com>
2025-06-24 01:26:48 +0800

    fix

--- qed_fermion/post_processors/benchmark_se_DD/benchmark_se_DD_Nrv100/plot_spsm_all_se.py
+++ qed_fermion/post_processors/benchmark_se_DD/benchmark_se_DD_Nrv100/plot_spsm_all_se.py
@@ -22,6 +22,7 @@ from load_write2file_convert import time_execution
 
 
 bid = 1
+plot_dqmc_postprocessing = True
 
 @time_execution
 def plot_spsm(Lsize=(6, 6, 10), bs=5, ipair=0):
@@ -39,55 +40,55 @@ def plot_spsm(Lsize=(6, 6, 10), bs=5, ipair=0):
     
     vs = Lx**2
     
-    
-    for J in Js:
-        # Initialize data collection arrays for this J value
-        all_data = []
-        
-        # Calculate the number of parts
-        num_parts = math.ceil((end_dqmc - start_dqmc) / part_size)
-        
-        # Loop through all batches and parts
-        for bid in range(bs):
-            # if not bid == 0: continue
-            for part_id in range(num_parts):
-                input_folder = root_folder + f"/run_meas_J_{J:.2g}_L_{Lx}_Ltau_{Ltau}_bid{bid}_part_{part_id}_psz_{part_size}_start_{start_dqmc}_end_{end_dqmc}/"
-                name = f"dimerx.bin" # dimer_dimer
-                ftdqmc_filename = os.path.join(input_folder, name)
-                
-                try:
-                    part_data = np.genfromtxt(ftdqmc_filename)
-                    all_data.append(part_data)
-                    print(f'Loaded ftdqmc data: {ftdqmc_filename}')
-                except (FileNotFoundError, ValueError) as e:
-                    raise RuntimeError(f'Error loading {ftdqmc_filename}: {str(e)}') from e
-        
-        # Combine all parts' data
-        data = np.concatenate(all_data)
-        data = data.reshape(bs, -1, vs, 4)  # [bs, seq, vs, values(kx, ky,spin, err)]
-        # data has shape [num_sample, vs, 4], where the last dim has entries: kx, ky, val, error. 
+    if plot_dqmc_postprocessing:
+        for J in Js:
+            # Initialize data collection arrays for this J value
+            all_data = []
+            
+            # Calculate the number of parts
+            num_parts = math.ceil((end_dqmc - start_dqmc) / part_size)
+            
+            # Loop through all batches and parts
+            for bid in range(bs):
+                # if not bid == 0: continue
+                for part_id in range(num_parts):
+                    input_folder = root_folder + f"/run_meas_J_{J:.2g}_L_{Lx}_Ltau_{Ltau}_bid{bid}_part_{part_id}_psz_{part_size}_start_{start_dqmc}_end_{end_dqmc}/"
+                    name = f"dimerx.bin" # dimer_dimer
+                    ftdqmc_filename = os.path.join(input_folder, name)
+                    
+                    try:
+                        part_data = np.genfromtxt(ftdqmc_filename)
+                        all_data.append(part_data)
+                        print(f'Loaded ftdqmc data: {ftdqmc_filename}')
+                    except (FileNotFoundError, ValueError) as e:
+                        raise RuntimeError(f'Error loading {ftdqmc_filename}: {str(e)}') from e
+            
+            # Combine all parts' data
+            data = np.concatenate(all_data)
+            data = data.reshape(bs, -1, vs, 4)  # [bs, seq, vs, values(kx, ky,spin, err)]
+            # data has shape [num_sample, vs, 4], where the last dim has entries: kx, ky, val, error. 
 
-        # Slice
-        data = data[:, start:, :, :]
+            # Slice
+            data = data[:, start:, :, :]
 
-        # spin order
-        spin_order = np.mean(data[..., 0, 2], axis=1)
-        spin_order_err = np.mean(np.abs(data[..., 0, 3]), axis=1)
-        spin_order_values.append(spin_order)
-        spin_order_errors.append(spin_order_err)
+            # spin order
+            spin_order = np.mean(data[..., 0, 2], axis=1)
+            spin_order_err = np.mean(np.abs(data[..., 0, 3]), axis=1)
+            spin_order_values.append(spin_order)
+            spin_order_errors.append(spin_order_err)
 
 
-    # ========== Spin order ========= #
-    # Stack spin_order_values and spin_order_errors to arrays of shape [Js_num, bs]
-    spin_order_values = np.stack(spin_order_values, axis=0)  # shape: [Js_num, bs]
-    spin_order_errors = np.stack(spin_order_errors, axis=0)  # shape: [Js_num, bs]
+        # ========== Spin order ========= #
+        # Stack spin_order_values and spin_order_errors to arrays of shape [Js_num, bs]
+        spin_order_values = np.stack(spin_order_values, axis=0)  # shape: [Js_num, bs]
+        spin_order_errors = np.stack(spin_order_errors, axis=0)  # shape: [Js_num, bs]
 
-    # Filter out outliers in spin_order_values (values > 100)
-    spin_order_values = np.where(spin_order_values > 20, np.nan, spin_order_values)
+        # Filter out outliers in spin_order_values (values > 100)
+        spin_order_values = np.where(spin_order_values > 20, np.nan, spin_order_values)
 
-    # Plot the batch mean
-    plt.errorbar(Js, spin_order_values[:, bid] / vs, yerr=spin_order_errors[:, bid] / vs,
-                 linestyle='-', marker='o', lw=2, color=f'C{i1}', label=f'hmc_{Lx}x{Ltau}')
+        # Plot the batch mean
+        plt.errorbar(Js, spin_order_values[:, bid] / vs, yerr=spin_order_errors[:, bid] / vs,
+                    linestyle='-', marker='o', lw=2, color=f'C{i1}', label=f'hmc_{Lx}x{Ltau}')
 
     
     # ---- Load and plot DD_k.pt mean ---- #
@@ -101,7 +102,7 @@ def plot_spsm(Lsize=(6, 6, 10), bs=5, ipair=0):
     DD_k0 = DD_k_res['DD_k_mean'][:, 0, 0]  # [J/bs, Ly, Lx]
     DD_k0_err = DD_k_res['DD_k_std'][:, 0, 0]
 
-    plt.errorbar(Js, DD_k0, yerr=DD_k0_err, 
+    plt.errorbar(Js, DD_k0 / vs, yerr=DD_k0_err / vs, 
                  fmt='o', color=f'C{i2}', linestyle='-', label=f'se_{Lx}x{Ltau}')
     
 
@@ -144,7 +145,7 @@ if __name__ == '__main__':
 
     # Plot setting
     plt.xlabel('J/t', fontsize=14)
-    plt.ylabel('DD_r=0', fontsize=14)
+    plt.ylabel('DD_k=0', fontsize=14)
     # plt.title(f'dimer_order vs J LxLtau={Lx}x{Ltau}', fontsize=16)
     plt.grid(True, alpha=0.3)
     plt.legend()
