722519d5d8432971572c98299d4df88e79b6d0f8
kxf <fengx463@alumni.umn.edu>
2025-07-03 00:35:48 +0800

    run_config

--- qed_fermion/pbs_files/r_large_cmp.sh
+++ qed_fermion/pbs_files/r_large_cmp.sh
@@ -21,7 +21,9 @@ J_array=$(echo '1.25')
 # L_array=$(echo '16 12 10')  # 10 h (-2)
 L_array=$(echo '40 36 30 20 16 12 10 8 6')
 L_array=$(echo '46 40') # 60 56 50 cuda kernel launch failure
-L_array=$(echo '50') # 60 56 50 cuda kernel launch failure
+L_array=$(echo '50')
+L_array=$(echo '56') 
+# L_array=$(echo '60 66') 
 
 # L_array=$(echo '24 20 16 12 10 8 6')
 
@@ -29,9 +31,10 @@ L_array=$(echo '50') # 60 56 50 cuda kernel launch failure
 # J_array=$(echo '1.0')
 # L_array=$(echo '6 8 10')  # 10 h (-2)
 
-Nstep=9000
+Nstep=10000
 export debug=0
 export cuda_graph=1
+export bs=1
 
 export suffix=cmp_large7_spsm
 export asym=1
