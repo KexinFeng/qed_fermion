0f8ec1d0383a686de631d5395e09d41dc0ded5a8
KexinFeng <23562091+KexinFeng@users.noreply.github.com>
2025-07-06 16:31:29 +0800

    plot

--- qed_fermion/scripts/draw_3d.py
+++ qed_fermion/scripts/draw_3d.py
@@ -33,13 +33,21 @@ for (i, j) in G.nodes():
         # Down bond
         if (i + 1, j) in G:
             edge = tuple(sorted([ (i, j), (i + 1, j) ]))
-            edge_colors[edge] = 'orange'
+            edge_colors[edge] = "purple"
 
 # Prepare edge color list in the order of G.edges()
 edge_color_list = [edge_colors[tuple(sorted(edge))] for edge in G.edges()]
 
 pos = {(x, y): (x, y) for x, y in G.nodes()}
-nx.draw(G, pos, edge_color=edge_color_list, node_size=50, width=2)
+nx.draw(G, pos, edge_color=edge_color_list, node_size=100, width=2)
+
+# Add orange circles at the midpoint of each edge
+for edge in G.edges():
+    (x0, y0) = pos[edge[0]]
+    (x1, y1) = pos[edge[1]]
+    mx, my = (x0 + x1) / 2, (y0 + y1) / 2
+    plt.scatter(mx, my, s=80, facecolors='orange', edgecolors='orange', linewidths=2, zorder=3)
+
 plt.axis('equal')
 # plt.show(block=False)
 
