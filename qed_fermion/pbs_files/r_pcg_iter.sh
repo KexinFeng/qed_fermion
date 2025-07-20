f2d218714f5df9a61e79f87f402723e7ee20e577
kxf <fengx463@alumni.umn.edu>
2025-07-21 02:21:44 +0800

    run_config

--- qed_fermion/pbs_files/r_pcg_iter.sh
+++ qed_fermion/pbs_files/r_pcg_iter.sh
@@ -23,8 +23,8 @@ L_array=$(echo '60 56 50')
 
 # L_array=$(echo '60 56 50 46 40 36 30 20 16 12 10 8 6')
 # L_array=$(echo '60 56 50 46 40 36 30 26 20 16 10')
-L_array=$(echo '30 20 10')
-# L_array=$(echo '60 50 40')
+L_array=$(echo '60 50 40 30 20 10')
+L_array=$(echo '60 40 20')
 max_iter_array=$(echo '200 400 800 1200')
 
 # J_array=$(echo '1.0')
