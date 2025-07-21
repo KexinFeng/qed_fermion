02c2249231e26effd4b9ede8b7aa26e3e8e6920e
kxf <fengx463@alumni.umn.edu>
2025-07-21 12:50:14 +0800

    fix

--- qed_fermion/hmc_sampler_batch.py
+++ qed_fermion/hmc_sampler_batch.py
@@ -972,6 +972,8 @@ class HmcSampler(object):
         delta_t_tensors = []
         for step in [5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]:
             file_name = f"boson_{self.specifics}_{step}.pt"
+            file_name = file_name.replace("cg_rtol_1e-15", "cg_rtol_1e-09")
+            file_name = file_name.replace(f"_max_iter_{self.max_iter}", "")
             file_path = os.path.join(data_folder, file_name)
             if os.path.exists(file_path):
                 loaded = torch.load(file_path)
@@ -3141,6 +3143,8 @@ class HmcSampler(object):
                 # Save the boson sequence to file every 500 steps
                 data_folder = script_path + f"/check_points/boson_ensemble/"
                 file_name = f"boson_{self.specifics}_{self.step-1}"
+                file_name = file_name.replace("cg_rtol_1e-15", "cg_rtol_1e-09")
+                file_name = file_name.replace(f"_max_iter_{self.max_iter}", "")
                 ckpt_data = {'boson': self.boson.cpu(),
                              'delta_t_tensor': self.delta_t_tensor.cpu()}
                 self.save_to_file(ckpt_data, data_folder, file_name)
