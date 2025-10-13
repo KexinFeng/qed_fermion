d2e34e0bf8d4c37e0264d18b96d0eb20a4198798
kxf <fengx463@alumni.umn.edu>
2025-10-13 18:26:56 +0800

    run_config

--- qed_fermion/pbs_files/r_large_noncmp.sh
+++ qed_fermion/pbs_files/r_large_noncmp.sh
@@ -35,26 +35,27 @@ L_array=$(echo '50')
 L_array=$(echo '36 30 20 16 12 10')
 # L_array=$(echo '46 40')
 # L_array=$(echo '60 56 50')
+L_array=$(echo '66')
 
 # J_array=$(echo '1.0')
 # L_array=$(echo '6 8 10')  # 10 h (-2)
 
-Nstep=8000
+Nstep=6500
 export debug=0
 export cuda_graph=1
-export cuda_graph_se=1
-export bs=2
+export cuda_graph_se=0
+export bs=1
 
-export suffix=ncmpK1_spsm_tau
+export suffix=ncmpK0_spsm_L66
 export asym=1
 export compact=0
-export K=1
+export K=0
 export dtau=0.1
 export precon=1
 
 export compute_BB=0
 export compute_spsm=0
-export compute_spsm_tau=1
+export compute_spsm_tau=0
 
 export seed=326
 
@@ -67,7 +68,7 @@ for L in $L_array; do
                 export J Nstep L
                 #
                 sbatch --job-name=${config} \
-                --time=3-23:59:00 \
+                --time=6-23:59:00 \
                 --qos=gpu \
                 --mem-per-cpu=8G \
                 s_hmc_noncmp.cmd
