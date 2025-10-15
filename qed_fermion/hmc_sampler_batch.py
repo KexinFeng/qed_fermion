3c6c1aedc3b8251c794b722c6d958ef83a1983c5
kxf <fengx463@alumni.umn.edu>
2025-10-15 11:19:25 +0800

    fix_total_monitor

--- qed_fermion/hmc_sampler_batch.py
+++ qed_fermion/hmc_sampler_batch.py
@@ -3275,7 +3275,7 @@ class HmcSampler(object):
         #     axes[0, 2].set_ylabel("Spsm_r_tau")
         #     axes[0, 2].set_title("spsm_r Over Steps")
         #     axes[0, 2].legend()
-        else:
+        elif compute_BB:
             # BB_r
             BB_r_mean = self.BB_r_list[seq_idx, ...].mean(axis=1)
             B_r_mean = self.B_r_list[seq_idx, ...].mean(axis=1)
