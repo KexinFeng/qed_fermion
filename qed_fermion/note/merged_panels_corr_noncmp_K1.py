255073bf66041549f60fd7cb561712ca287102ae
KexinFeng <23562091+KexinFeng@users.noreply.github.com>
2025-10-20 09:35:35 +0800

    rename

--- qed_fermion/note/merged_panels_corr_noncmp_K1.py
+++ qed_fermion/note/merged_panels_corr_noncmp_K1.py
@@ -47,7 +47,7 @@ def main():
 
     save_dir = os.path.join(script_path, 'figures')
     os.makedirs(save_dir, exist_ok=True)
-    out_path_pdf = os.path.join(save_dir, 'fermion_corr_noncmp_K1.pdf')
+    out_path_pdf = os.path.join(save_dir, 'fermion_corr_noncmp_K1_tmp.pdf')
     plt.savefig(out_path_pdf, bbox_inches='tight')
     print(f'Saved merged figure to: {out_path_pdf}')
 
