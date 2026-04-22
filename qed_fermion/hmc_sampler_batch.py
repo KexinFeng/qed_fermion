4db17fa9097ef955f11080558d656fad435f54cc
kexin <fengx463@alumni.umn.edu>
2026-04-22 04:22:16 -0500

    fix

--- qed_fermion/hmc_sampler_batch.py
+++ qed_fermion/hmc_sampler_batch.py
@@ -3408,10 +3408,14 @@ def load_visualize_final_obsr(Lsize=(20, 20, 20), step=1000001,
     start = start_load
     end = step
     sample_step = 1
-    if start <= end:
-        print(f"upper bound and larger bound inconsistent with step sign.start: {start} <= end: {end}")
+
+    try:
+        seq_idx = torch.arange(start, end, sample_step)
+    except Exception as err:
+        print(err)
+        print(f"start:{start}, end:{end}, step:{step}")
         return
-    seq_idx = torch.arange(start, end, sample_step)
+    
     # batch_idx = torch.tensor([0, 1, 2, 3, 4])
     batch_size = G_list.size(1)
     batch_idx = torch.arange(batch_size)
